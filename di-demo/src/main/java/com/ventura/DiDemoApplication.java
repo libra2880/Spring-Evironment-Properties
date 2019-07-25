626a9da6fe7e6dbf2f2649dd3141e25c6cd21aa2
JIMMY <JIMMY@DESKTOP-M1938FH>
2019-07-24 20:24:28 -0500

    94.-Property Source

--- di-demo/src/main/java/com/ventura/DiDemoApplication.java
+++ di-demo/src/main/java/com/ventura/DiDemoApplication.java
@@ -9,6 +9,7 @@ import com.ventura.controller.ConstructorInjectedController;
 import com.ventura.controller.GetterInjectedController;
 import com.ventura.controller.MyController;
 import com.ventura.controller.PropertyInjectedController;
+import com.ventura.ejemplobean.FakeDataSource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ventura","com.ventura.service"})
@@ -17,12 +18,16 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx=SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller=(MyController) ctx.getBean("myController");
-		
-		
-		System.out.println(controller.hola());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).decirHola());
-		System.out.println(ctx.getBean(GetterInjectedController.class).decirHola());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).decirHola());
+		FakeDataSource fakeDataSource=(FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+		/*
+		 * System.out.println(controller.hola());
+		 * System.out.println(ctx.getBean(PropertyInjectedController.class).decirHola())
+		 * ;
+		 * System.out.println(ctx.getBean(GetterInjectedController.class).decirHola());
+		 * System.out.println(ctx.getBean(ConstructorInjectedController.class).decirHola
+		 * ());
+		 */
 	
 	}
 
